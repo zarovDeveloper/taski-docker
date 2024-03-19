a3a8e2e58b54b250a1dfc00c294b53263f18d555
zarovDeveloper <zarovDeveloper@gmail.com>
2024-03-19 17:12:08 +0300

    Update db in workflow

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -9,12 +9,10 @@ class TaskiAPITestCase(TestCase):
         self.guest_client = Client()
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
-        """Проверка создания задачи."""
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
